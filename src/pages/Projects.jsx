ef7a45430ab9e54fe93a51bca0f2cdb3991fdbc6
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-10-19 18:04:09 +0700

    Refactor image URLs in Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,7 +7,7 @@ const Portfolio = () => {
                <div className="m-10 text-center">
                     <h1 className="text-4xl font-bold text-orange-500 p-2">My Projects</h1>
                </div>
-               <div className="max-w-7xl px-5 lg:px-10 md:px-7 gap-x-4 gap-y-4 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+               <div className="max-w-7xl mx-auto px-5 lg:px-10 md:px-7 gap-x-4 gap-y-4 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
 
                     {apiPortfolio.map((item) => (
                          <a href={item.url} key={item.id} className="card bg-base-100 shadow-xl">
