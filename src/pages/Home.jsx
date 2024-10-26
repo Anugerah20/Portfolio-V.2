7d7b4010b521feb07a21af1db2be5968f2f4da71
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-10-26 15:24:28 +0700

    Update Home.jsx to fix button alignment issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,7 +31,7 @@ const Card = () => {
           <>
                <section className="flex flex-col px-8 py-20 bg-base-200 min-h-screen">
                     <main className="flex">
-                         <div className="max-w-7xl">
+                         <div className="w-full">
                               <h1 className="text-3xl font-bold my-5">Hey, I&apos;m <span className="border-b-2 border-orange-500">Nabil <span className="text-orange-500">Anugerah Pangestu</span></span></h1>
                               <h1 className="py-3 lg:text-6xl text-4xl font-semibold">
                                    <span className="text-orange-500">I specialize</span> in both building <span className="text-orange-500">web interfaces</span> and <span className="text-orange-500">backend systems.</span>
@@ -40,11 +40,9 @@ const Card = () => {
                               <div className="flex mt-2">
                                    <button onClick={downloadFile} className="btn btn-outline">Download CV</button>
                               </div>
-
                               <span className="flex justify-center text-center animate-bounce mt-14">
-                                   <button onClick={scrollToPortfolio} className="flex items-center gap-2 text-md font-bold transition hover:delay-100 hover:text-orange-500"> <IoChevronDown /> Show Projects</button>
+                                   <button onClick={scrollToPortfolio} className="text-center flex justify-center items-center gap-2 text-md font-bold transition hover:delay-100 hover:text-orange-500"> <IoChevronDown /> Show Projects</button>
                               </span>
-
                          </div>
                     </main>
                </section>
