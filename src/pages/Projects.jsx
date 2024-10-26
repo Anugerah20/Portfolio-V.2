ff70dcec6b4f2a24866ee355c88e378f5438a681
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-10-26 15:24:39 +0700

    Update Projects.jsx to fix project card styling

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,10 +6,10 @@ const Portfolio = () => {
                <div className="m-10 text-center">
                     <h1 className="text-4xl font-bold text-orange-500 p-2">My Projects</h1>
                </div>
-               <div className="max-w-7xl mx-auto px-5 lg:px-10 md:px-7 gap-x-4 gap-y-4 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+               <div className="max-w-7xl mx-auto my-5 px-5 lg:px-10 md:px-7 gap-x-4 gap-y-4 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
 
                     {apiPortfolio.map((item) => (
-                         <a href={item.url} key={item.id} className="card bg-base-100 shadow-xl">
+                         <a href={item.url} key={item.id} className="card bg-[#191e24] shadow-xl">
 
                               <figure>
                                    <img
