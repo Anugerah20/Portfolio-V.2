0e6e535ced7aaa1f02ec32c7f0b8af4d4344bf4b
NabilEstu <nabilanugerah20@gmail.com>
2025-08-11 10:47:25 +0700

    feat(parallax/main): add parallax provider/main

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Toaster } from 'sonner';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ParallaxProvider>
+      <App />
+    </ParallaxProvider>
     <Toaster position="top-left" />
   </StrictMode>,
 )
