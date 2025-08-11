5e61c60449c78f6cf56fa760db7544e39fd75a66
NabilEstu <nabilanugerah20@gmail.com>
2025-08-11 11:22:06 +0700

    update(styling/css): styling responsive page contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,37 +2,70 @@ import { FaLinkedin, FaGithub, FaInstagram, FaEnvelope } from "react-icons/fa";
 
 const Contact = () => {
      return (
-          <section className="m-10 flex justify-center items-center">
-               <div className="max-w-full">
+          <section className="m-4 md:m-10 flex justify-center items-center">
+               <div className="w-full max-w-4xl">
                     <div className="text-center">
-                         <h1 className="text-4xl font-bold text-orange-500 mb-10">Get in Touch</h1>
+                         <h1 className="text-2xl md:text-4xl font-bold text-orange-500 mb-6 md:mb-10">
+                              Get in Touch
+                         </h1>
                     </div>
-                    <div className="grid grid-cols-1 lg:grid-cols-2 space-x-10 mx-20 border border-gray-700 rounded-md p-10">
-                         <div className="text-center lg:text-left lg:text-4xl md:text-3xl text-2xl font-bold">
-                              <span className="block leading-tight">Want to <span className="text-orange-500">Hire Me?</span></span>
+
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 md:gap-10 border border-gray-700 rounded-md p-4 md:p-10">
+                         <div className="text-center lg:text-left text-xl md:text-3xl lg:text-4xl font-bold">
+                              <span className="block leading-tight">
+                                   Want to <span className="text-orange-500">Hire Me?</span>
+                              </span>
                               <span className="block leading-tight">Interested in Working</span>
                               <span className="block leading-tight">Together</span>
-                              <span className="block leading-tight">or Discuss <span className="text-orange-500">With Me?</span></span>
+                              <span className="block leading-tight">
+                                   or Discuss <span className="text-orange-500">With Me?</span>
+                              </span>
+
                               <div className="flex justify-center lg:justify-start mt-4">
-                                   <a href="mailto:nabilanugerah20@gmail.com" className="btn btn-neutral flex items-center gap-2">
-                                        <FaEnvelope className="text-2xl" /> Hire Me
+                                   <a
+                                        href="mailto:nabilanugerah20@gmail.com"
+                                        className="btn btn-neutral md:btn-md flex items-center gap-2 text-sm md:text-base"
+                                   >
+                                        <FaEnvelope className="text-xl md:text-1xl" /> Hire Me
                                    </a>
                               </div>
                          </div>
-                         <div>
-                              <ul className="menu rounded-box">
-                                   <h1 className="lg:text-4xl md:text-3xl text-2xl font-bold text-orange-500 text-left lg:text-left ml-3">Contact Information</h1>
-                                   <li className="lg:text-xl md:text-xl text-lg font-bold flex items-start gap-3 mt-3">
-                                        <a href="mailto:nabilanugerah20@gmail.com"><FaEnvelope className="text-3xl" /> nabilanugerah20@gmail.com</a>
+                         <div className="mb-8 flex flex-col flex-wrap justify-center items-center">
+                              <h1 className="text-xl md:text-3xl lg:text-4xl font-bold text-orange-500 text-left ml-1 md:ml-3">
+                                   Contact Information
+                              </h1>
+                              <ul className="mt-3 space-y-3">
+                                   <li className="flex items-start gap-3 text-base md:text-xl font-bold">
+                                        <a
+                                             href="mailto:nabilanugerah20@gmail.com"
+                                             className="flex items-center gap-3"
+                                        >
+                                             <FaEnvelope className="text-2xl md:text-3xl" /> nabilanugerah20@gmail.com
+                                        </a>
                                    </li>
-                                   <li className="lg:text-xl md:text-xl text-lg font-bold flex items-start gap-3 mt-3">
-                                        <a href="https://github.com/Anugerah20/"><FaGithub className="text-3xl" /> Anugerah20</a>
+                                   <li className="flex items-start gap-3 text-base md:text-xl font-bold">
+                                        <a
+                                             href="https://github.com/Anugerah20/"
+                                             className="flex items-center gap-3"
+                                        >
+                                             <FaGithub className="text-2xl md:text-3xl" /> Anugerah20
+                                        </a>
                                    </li>
-                                   <li className="lg:text-xl md:text-xl text-lg font-bold flex items-start gap-3 mt-3">
-                                        <a href="https://instagram.com/anugerah_est/"><FaInstagram className="text-3xl" /> anugerah_est</a>
+                                   <li className="flex items-start gap-3 text-base md:text-xl font-bold">
+                                        <a
+                                             href="https://instagram.com/anugerah_est/"
+                                             className="flex items-center gap-3"
+                                        >
+                                             <FaInstagram className="text-2xl md:text-3xl" /> anugerah_est
+                                        </a>
                                    </li>
-                                   <li className="lg:text-xl md:text-xl text-lg font-bold flex items-start gap-3 mt-3">
-                                        <a href="https://www.linkedin.com/in/nabil-anugerah-pangestu/"><FaLinkedin className="text-3xl" /> Nabil Anugerah Pangestu</a>
+                                   <li className="flex items-start gap-3 text-base md:text-xl font-bold">
+                                        <a
+                                             href="https://www.linkedin.com/in/nabil-anugerah-pangestu/"
+                                             className="flex items-center gap-3"
+                                        >
+                                             <FaLinkedin className="text-2xl md:text-3xl" /> Nabil Anugerah Pangestu
+                                        </a>
                                    </li>
                               </ul>
                          </div>
